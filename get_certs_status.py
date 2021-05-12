eaccc60686538dbb404256976cad91d3701eb01b
Marcin Wojtczuk <marcin.wojtczuk@pl.bosch.com>
2021-05-12 12:20:48 +0200

    Move to function to make space for chaining CRD objects - we need a chain

--- get_certs_status.py
+++ get_certs_status.py
@@ -3,6 +3,7 @@ import time
 from kubernetes.client.rest import ApiException
 from pprint import pprint
 import json
+import argparse
 
 
 def get_crds(api_instance):
@@ -22,19 +23,77 @@ def get_crds(api_instance):
     
     return ret
 
+
+def get_problematic_certificaterequests(api_instance, knamespace=None):
+    kgroup      = "cert-manager.io"
+    kversion    = "v1"
+    kplural     = "certificaterequests"
+    kshort       = "crs"
+
+    api_instance = client.CustomObjectsApi(api_client)
+    problematic_crs = []
+
+    api_response = api_instance.list_cluster_custom_object(kgroup, kversion, kplural)
+    resp = dict(api_response.items())
+    for crs in resp['items']:
+        crs_name        = crs['metadata']['name']
+        crs_namespace   = crs['metadata']['namespace']
+        crs_status = "Unknown status"
+        crs_ok = False
+
+        if knamespace and knamespace != crs_namespace:
+            continue    # only include objects from namespace set in params
+
+        if crs.get('status'):
+            crs_reason  = crs['status']['conditions'][0]['reason']
+            crs_msg     = crs['status']['conditions'][0]['message']
+            crs_status  = f'{crs_reason}: ' + crs_msg
+            crs_ok      = (crs['status']['conditions'][0]['status'] == 'True')
+        
+        if not crs_ok:
+            problematic_crs.append([crs_namespace, crs_name, crs_status])
+
+    return sorted(problematic_crs, key=lambda x: (x[0],x[1]))
+
+
 if __name__ == '__main__':
-    configuration = config.load_kube_config(context='blue.k8s.dev.connected-biking.cloud')
+    parsea = argparse.ArgumentParser()
+    parsea.add_argument('cluster', help='Context name from ~/kube/config')
+    parsea.add_argument('-n' , '--namespace', help='Optional namespace to use as filter')
+    args = parsea.parse_args()
+    
+    configuration = config.load_kube_config(context=args.cluster)
 
     with client.ApiClient(configuration) as api_client:
         api_instance = client.ApiextensionsV1Api(api_client)
         
-        try:
-            h_col1 = 'NAME'
-            h_col2 = 'APP LABEL'
-            print(f'{h_col1:<60}{h_col2}')
+        # Print Custom Resource Definition objects on the cluster:
+        # try:
+        #     h_col1 = 'CRD NAME'
+        #     h_col2 = 'CRD\'s APP LABEL'
+        #     print(f'{h_col1:<60}{h_col2}')
             
-            for crd in get_crds(api_instance):
-                name, app = (crd['name'], crd.get('app'))
-                print(f'{name:<60}{app}')
+        #     for crd in get_crds(api_instance):
+        #         name, app = (crd['name'], crd.get('app'))
+        #         print(f'{name:<60}{app}')
+        # except ApiException as e:
+        #     print("Exception when calling ApiextensionsV1Api->list_custom_resource_definition: %s\n" % e)
+
+        # Print problematic CertificateRequests
+        print('Problematic CertificateRequests')
+        try:
+            problematic_crs = get_problematic_certificaterequests(api_client, args.namespace)
+            h_col1 = 'NAMESPACE'
+            h_col2 = 'CertificateRequest'.upper()
+            h_col3 = 'STATUS'
+            print(f'{h_col1:<20}{h_col2:<64}{h_col3}')
+            for problem in problematic_crs:
+                cn      = problem[0]
+                cname   = problem[1]
+                creason = problem[2][:100]
+                print(f'{cn:<20}{cname:<64}{creason}')
         except ApiException as e:
-            print("Exception when calling ApiextensionsV1Api->list_custom_resource_definition: %s\n" % e)
+            print("Exception when calling CustomObjectsApi->list_cluster_custom_object: %s\n" % e)
+
+    # Automatic fixing of expired certs:
+    # find crs, cert, order and secret that come from the same crequest and delete them in following order: secret, cert, order, crs
